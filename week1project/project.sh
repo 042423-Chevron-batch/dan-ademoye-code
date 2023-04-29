cdda198a3394eea6ae47d1178ffcb5e3ef2c6b4f
Ademoye123 <94658956+Ademoye123@users.noreply.github.com>
2023-04-29 11:54:23 -0400

    created functions Rock Paper scissors

--- week1project/project.sh
+++ week1project/project.sh
@@ -1,1 +1,32 @@
-daniel started 
+#!/bin/bash
+
+# an array of options for the game
+options=("rock" "paper" "scissors")
+
+# function to generate a random choice for the computer
+function get_computer_choice {
+    computer_choice=${options[$((RANDOM % 3))]}
+    echo $computer_choice
+}
+
+# function to compare the choices and determine the winner
+function get_winner {
+    if [[ $1 == $2 ]]; then
+        echo "It's a tie!"
+    elif [[ $1 == "rock" && $2 == "scissors" ]] || [[ $1 == "paper" && $2 == "rock" ]] || [[ $1 == "scissors" && $2 == "paper" ]]; then
+        echo "You win!"
+    else
+        echo "Computer wins!"
+    fi
+}
+
+# function to ask the user if they want to play again
+function ask_play_again {
+    read -p "Do you want to play again? (yes/no): " answer
+    if [[ $answer == "yes" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
