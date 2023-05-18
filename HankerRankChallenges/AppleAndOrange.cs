c213f0e089523df61418d3b564f9c47bb8eb76ef
Ademoye123 <94658956+Ademoye123@users.noreply.github.com>
2023-05-18 10:38:19 -0400

    modified AppleAndOrange CS

--- HankerRankChallenges/AppleAndOrange.cs
+++ HankerRankChallenges/AppleAndOrange.cs
@@ -1,1 +1,22 @@
+     static void countApplesAndOranges(int s, int t, int a, int b, List<int> apples, List<int> oranges)
+    {
+        // Start the count for apples and oranges at zero.
+        int countApples = 0;
+        int countOranges = 0;
+        
+        //increment countApples if the apple falls within the distance between s and t
+        for(int i = 0; i < apples.Count; i++)
+        {
+            if(a + apples[i] >= s && a + apples[i] <= t)
+                apples++;
+        }    
+        //increment countOranges if the orange falls within the distance between s and t
+        for(int r = 0; r < oranges.Count; r++)
+        {
+            if(b + oranges[r] >= s && b + oranges[r] <= t)
+                oranges++;
+    }   //display the count for apples and oranges on different lines
+        Console.Writeline(countApples);
+        Console.Writeline(countOranges);
 
+}
