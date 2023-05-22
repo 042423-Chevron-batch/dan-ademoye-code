8b684fcfc746dfda0e1ab56928826d5f18d98958
Ademoye123 <94658956+Ademoye123@users.noreply.github.com>
2023-05-22 14:07:41 -0400

    modified

--- ProjectStoreApi/ProjectApiStoreAPi/ConsoleUI.cs
+++ ProjectStoreApi/ProjectApiStoreAPi/ConsoleUI.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.data.SqlClient;
+using System.Data.SqlClient;
 using ProjectStoreApiModels;
 using ProjectStoreApiRepository;
 
@@ -10,117 +10,129 @@ namespace ProjectApiStoreAPi
 {
     public class ConsoleUI
     {
-    public void Run()
-    {
-        bool isLoggedIn = false;
-        string selectedStore = null;
+        private bool isLoggedIn = false;
+        private string selectedStore = null;
 
-        while (true)
+        public void Run()
         {
-            Console.Clear();
-            if (!isLoggedIn)
+            while (true)
             {
-                Console.WriteLine("Welcome to the Store Application!");
-                Console.WriteLine("---------------------------------");
-                Console.WriteLine("Please log in to continue:");
-                Console.Write("Username: ");
-                string username = Console.ReadLine();
-                Console.Write("Password: ");
-                string password = Console.ReadLine();
+                Console.Clear();
 
-                // Perform authentication and validation logic
-                bool isLoginValid = AuthenticateUser(username, password);
-
-                if (isLoginValid)
+                if (!isLoggedIn)
                 {
-                    isLoggedIn = true;
-                    Console.WriteLine("Login successful!");
-                    Console.WriteLine("Press any key to continue.");
-                    Console.ReadKey();
+                    Login();
                 }
                 else
                 {
-                    Console.WriteLine("Invalid username or password. Please try again.");
-                    Console.WriteLine("Press any key to continue.");
-                    Console.ReadKey();
+                    SelectStore();
+                    break;
                 }
+            }
+
+            // TODO After store selection, you can proceed with other functionalities
+            // TODO such as viewing products and adding them to the cart
+        }
+
+        private void Login()
+        {
+            Console.WriteLine("Welcome to the Store Application!");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Please log in to continue:");
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            // Perform authentication and validation logic
+            bool isLoginValid = AuthenticateUser(username, password);
+
+            if (isLoginValid)
+            {
+                isLoggedIn = true;
+                Console.WriteLine("Login successful!");
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
             }
             else
             {
-                Console.Clear();
-                Console.WriteLine("Welcome to the Store Application!");
-                Console.WriteLine("---------------------------------");
-                Console.WriteLine("Please choose a store location:");
-                Console.WriteLine("1. Jacksville Location 1");
-                Console.WriteLine("2. Mark Location 2");
-                Console.WriteLine("3. Florida Location 3");
-                Console.WriteLine("4. Maryland Location 4");
-                Console.WriteLine("5. Houston Location 5");
-                Console.Write("Enter the number corresponding to your desired store: ");
-                string storeInput = Console.ReadLine();
-
-                // Perform input validation and store selection logic
-                if (int.TryParse(storeInput, out int storeNumber) && storeNumber >= 1 && storeNumber <= 5)
-                {
-                    // Map store number to store location or ID
-                    selectedStore = GetStoreLocation(storeNumber);
-
-                    Console.WriteLine($"You have selected {selectedStore}.");
-                    Console.WriteLine("Press any key to continue.");
-                    Console.ReadKey();
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Please try again.");
-                    Console.WriteLine("Press any key to continue.");
-                    Console.ReadKey();
-                }
+                Console.WriteLine("Invalid username or password. Please try again.");
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
             }
         }
 
-        // After store selection, TODO with other functionalities
-        // viewing products, adding to cart
-    }
-
-    private bool AuthenticateUser(string username, string password)
-    {
-        // Perform authentication logic and validate credentials
-        // Return true if the username and password are valid, false otherwise
-        // let's assume the username is "admin" and the password is "password"
-        return username == "admin" && password == "password";
-    }
-
-    private string GetStoreLocation(int storeNumber)
-    {
-        // Map the store number to the corresponding store location or ID
-        // You can retrieve the store location from your data store or use a predefined mapping
-        // let's assume the store locations are as follows:
-        switch (storeNumber)
+        private bool AuthenticateUser(string username, string password)
         {
-            case 1:
-                return "Jacksville Location 1";
-            case 2:
-                return "Mark Location 2";
-            case 3:
-                return "florida Location 3";
-            case 4:
-                return "maryland Location 4";
-            case 5:
-                return "Houston Location 5";
-            default:
-                return string.Empty;
+            // Perform authentication logic and validate credentials
+            // Return true if the username and password are valid, false otherwise
+            // Let's assume the username is "admin" and the password is "password"
+            return username == "admin" && password == "password";
+        }
+
+        private void SelectStore()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the Store Application!");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Please choose a store location:");
+            Console.WriteLine("1. Jacksville Location 1");
+            Console.WriteLine("2. Mark Location 2");
+            Console.WriteLine("3. Florida Location 3");
+            Console.WriteLine("4. Maryland Location 4");
+            Console.WriteLine("5. Houston Location 5");
+            Console.Write("Enter the number corresponding to your desired store: ");
+            string storeInput = Console.ReadLine();
+
+            // Perform input validation and store selection logic
+            if (int.TryParse(storeInput, out int storeNumber) && storeNumber >= 1 && storeNumber <= 5)
+            {
+                // Map store number to store location or ID
+                selectedStore = GetStoreLocation(storeNumber);
+
+                Console.WriteLine($"You have selected {selectedStore}.");
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+                SelectStore(); // Retry store selection
+            }
+        }
+
+        private string GetStoreLocation(int storeNumber)
+        {
+            // Map the store number to the corresponding store location or ID
+            // You can retrieve the store location from your data store or use a predefined mapping
+            // Let's assume the store locations are as follows:
+            switch (storeNumber)
+            {
+                case 1:
+                    return "Jacksville Location 1";
+                case 2:
+                    return "Mark Location 2";
+                case 3:
+                    return "Florida Location 3";
+                case 4:
+                    return "Maryland Location 4";
+                case 5:
+                    return "Houston Location 5";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+
+    public class StoreService
+    {
+        public static void Main()
+        {
+            ConsoleUI consoleUI = new ConsoleUI();
+            consoleUI.Run();
         }
     }
 }
 
-public class StoreService
-{
-    public static void Main()
-    {
-        ConsoleUI consoleUI = new ConsoleUI();
-        consoleUI.Run();
-    }
-}
-
-    }
--- ProjectStoreApi/ProjectStoreApiBusiness/StoreService.cs
+++ ProjectStoreApi/ProjectStoreApiBusiness/StoreService.cs
@@ -9,6 +9,99 @@ namespace ProjectStoreApiBusiness
 {
     public class StoreService
     {
+        // Function for user to login 
+        private void Login()
+        {
+            Console.WriteLine("Welcome to the Store Application!");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Please log in to continue:");
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            // Perform authentication and validation logic
+            bool isLoginValid = AuthenticateUser(username, password);
+
+            if (isLoginValid)
+            {
+                isLoggedIn = true;
+                Console.WriteLine("Login successful!");
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Invalid username or password. Please try again.");
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+            }
+        }
+
+        private bool AuthenticateUser(string username, string password)
+        {
+            // Perform authentication logic and validate credentials
+            // Return true if the username and password are valid, false otherwise
+            // Let's assume the username is "admin" and the password is "password"
+            return username == "admin" && password == "password";
+        }
+
+        private void SelectStore()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the Store Application!");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Please choose a store location:");
+            Console.WriteLine("1. Jacksville Location 1");
+            Console.WriteLine("2. Mark Location 2");
+            Console.WriteLine("3. Florida Location 3");
+            Console.WriteLine("4. Maryland Location 4");
+            Console.WriteLine("5. Houston Location 5");
+            Console.Write("Enter the number corresponding to your desired store: ");
+            string storeInput = Console.ReadLine();
+
+            // Perform input validation and store selection logic
+            if (int.TryParse(storeInput, out int storeNumber) && storeNumber >= 1 && storeNumber <= 5)
+            {
+                // Map store number to store location or ID
+                selectedStore = GetStoreLocation(storeNumber);
+
+                Console.WriteLine($"You have selected {selectedStore}.");
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey();
+                SelectStore(); // Retry store selection
+            }
+        }
+             // Funcion to get store location
+        private string GetStoreLocation(int storeNumber)
+        {
+            // Map the store number to the corresponding store location or ID
+            // You can retrieve the store location from your data store or use a predefined mapping
+            // Let's assume the store locations are as follows:
+            switch (storeNumber)
+            {
+                case 1:
+                    return "Jacksville Location 1";
+                case 2:
+                    return "Mark Location 2";
+                case 3:
+                    return "Florida Location 3";
+                case 4:
+                    return "Maryland Location 4";
+                case 5:
+                    return "Houston Location 5";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+        // function to Add customer 
        public void AddCustomer(Customer customer)
        {
     // Validate input
@@ -18,9 +111,10 @@ namespace ProjectStoreApiBusiness
     }
     
     // Save the customer to the database
-    CustomerRepository.Add(customer);
-    }
-    public List<Customer> SearchCustomersByName(string name)
+   // CustomerRepository.Add(customer);
+       }
+       // function to Search Customer by name
+    public void SearchCustomersByName(string name)
     {
     // Validate input
     if (string.IsNullOrEmpty(name))
@@ -29,8 +123,9 @@ namespace ProjectStoreApiBusiness
     }
 
     // Search for customers in the database
-    return CustomerRepository.GetByName(name);
+   // return CustomerRepository.GetByName(name);
     }
+    // Function to Display Order Details
     public void DisplayOrderDetails(Order order)
     {
     // Validate input
@@ -40,7 +135,7 @@ namespace ProjectStoreApiBusiness
     }
 
     // Display order details
-    Console.WriteLine($"Order ID: {order.ID}");
+    //Console.WriteLine($"Order ID: {order.ID}");
     Console.WriteLine($"Order Time: {order.OrderTime}");
     Console.WriteLine($"Customer: {order.Customer.FirstName} {order.Customer.LastName}");
     Console.WriteLine($"Store Location: {order.StoreLocation.Name}");
@@ -51,7 +146,7 @@ namespace ProjectStoreApiBusiness
         Console.WriteLine($"- {product.Name} (${product.Price})");
     }
 }
-
+     // Function to Display lacation order history
     public void DisplayStoreOrderHistory(Location storeLocation)
 {
     // Input validation
@@ -61,13 +156,13 @@ namespace ProjectStoreApiBusiness
     }
 
     // Get the order history from the database
-    List<Order> orders = OrderRepository.GetOrdersByLocation(storeLocation);
+    //List<Order> orders = OrderRepository.GetOrdersByLocation(storeLocation);
 
     // Display order history
     Console.WriteLine($"Order History of {storeLocation.Name}:");
-    foreach (Order order in orders)
+    foreach (Order order in Orders)
     {
-        Console.WriteLine($"Order ID: {order.ID}");
+        //Console.WriteLine($"Order ID: {order.ID}");
         Console.WriteLine($"Order Time: {order.OrderTime}");
         Console.WriteLine($"Customer: {order.Customer.FirstName} {order.Customer.LastName}");
         Console.WriteLine("Products:");
@@ -79,6 +174,7 @@ namespace ProjectStoreApiBusiness
     }
     
 }
+    // Function to Display customer history
         public void DisplayCustomerOrderHistory(Customer customer)
 {
     // Input validation
@@ -88,7 +184,7 @@ namespace ProjectStoreApiBusiness
     }
 
     // Get the order history from the database
-    List<Order> orders = OrderRepository.GetOrdersBy;
+    //List<Order> orders = OrderRepository.GetOrdersBy;
 
     }
 
@@ -118,11 +214,8 @@ namespace ProjectStoreApiBusiness
             Console.WriteLine("Invalid quantity. Please try again.");
         }
 
-        Console.WriteLine("-------------------------");
-        ChooseStore();
-    }  
-    
- }
+        
+    }
+}    
 
-}
 
